309019a08333258662805ad937366d074b046218
Bhanu Pratap Reddy <bhanuyash@gmail.com>
2023-12-16 12:03:55 +0530

    first commit

--- AI_image_gen.py
+++ AI_image_gen.py
@@ -18,16 +18,18 @@ class ConsoleRedirector(io.StringIO):
         self.text_widget.insert(tk.END, output)
         self.text_widget.see(tk.END)
 
-def save_image(image, path='AI Images/Images'):
+def save_image(image, path='Image-generator-stable-diffusion/Images'):
     if not os.path.exists(path):
         os.makedirs(path)
     image.save(os.path.join(path, "generated_image.png"))
 
 def generate_and_display_image(prompt, label):
     def generate():
+        global pil_image
         with torch.no_grad():
             image = pipe(prompt=prompt).images[0]
         image = Image.fromarray(image) if not isinstance(image, Image.Image) else image
+        pil_image = image.copy()
         tk_image = ImageTk.PhotoImage(image)
         label.config(image=tk_image)
         label.image = tk_image
@@ -52,15 +54,21 @@ text_input.pack(pady=10)
 label = ttk.Label(root)
 label.pack(pady=10)
 
-save_button = ttk.Button(root, text="Save Image", command=lambda: save_image(label.image))
+pil_image = None
+
+def save_generated_image():
+    if pil_image is not None:
+        save_image(pil_image)
+    else:
+        print("No image to save")
+
+save_button = ttk.Button(root, text="Save Image", command=save_generated_image)
 save_button.pack(pady=10)
 
 text_input.bind("<Return>", lambda event, lbl=label: on_enter_pressed(event, lbl))
 
 text_widget = tk.Text(root, height=10)
 text_widget.pack()
-print("This is a test message before redirection.")
 sys.stdout = ConsoleRedirector(text_widget)
-print("This is a test message after redirection.")
 
 root.mainloop()
